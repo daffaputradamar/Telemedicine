b37d4d1032b3b6a1f153c4fe61385c48feba303e
Daffa Akbar <daffaputradamar@gmail.com>
2020-07-23 13:19:20 +0700

    add dokter list page

--- web/src/pages/DoctorList/DoctorList.js
+++ web/src/pages/DoctorList/DoctorList.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useContext, useState } from "react";
 import { CssBaseline, Container } from "@material-ui/core";
 import Navbar from "../../components/Navbar";
+import { FirebaseContext } from "../../components/Firebase";
+import DoctorTable from "./DoctorTable";
 
 function DoctorList() {
+  const firebaseContext = useContext(FirebaseContext);
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
+
+  const fetchDoctors = () => {
+    firebaseContext.refDoctors().on("value", (snapshot) => {
+      const val = snapshot.val();
+      const _doctors = Object.keys(val).map((id) => ({ uid: id, ...val[id] }));
+      setDoctors(_doctors);
+    });
+  };
+
   return (
     <Fragment>
       <CssBaseline />
       <Navbar />
-      <Container>tes</Container>
+      <Container>{doctors && <DoctorTable rows={doctors} />}</Container>
     </Fragment>
   );
 }
