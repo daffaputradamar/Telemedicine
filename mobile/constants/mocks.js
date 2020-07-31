13ff5ee0939b9c0945381f769734525bf2e58f22
Daffa Akbar <daffaputradamar@gmail.com>
2020-07-31 08:19:57 +0700

    cleaning up unused assets

--- mobile/constants/mocks.js
+++ mobile/constants/mocks.js
@@ -5,7 +5,6 @@ const documents = [
     tags: ["products", "inspirations"],
     count: 147,
     reply: "lorem ipsum blablablba",
-    image: require("../assets/icons/plants.png"),
   },
   {
     id: "seeds",
@@ -13,7 +12,6 @@ const documents = [
     tags: ["products", "shop"],
     count: 16,
     reply: "",
-    image: require("../assets/icons/seeds.png"),
   },
   {
     id: "flowers",
@@ -21,7 +19,6 @@ const documents = [
     tags: ["products", "inspirations"],
     count: 68,
     reply: "lorem ipsum blablablba",
-    image: require("../assets/icons/flowers.png"),
   },
   {
     id: "sprayers",
@@ -29,7 +26,6 @@ const documents = [
     tags: ["products", "shop"],
     count: 17,
     reply: "lorem ipsum blablablba",
-    image: require("../assets/icons/sprayers.png"),
   },
   {
     id: "pots",
@@ -37,7 +33,6 @@ const documents = [
     tags: ["products", "shop"],
     count: 47,
     reply: "lorem ipsum blablablba",
-    image: require("../assets/icons/pots.png"),
   },
   {
     id: "fertilizers",
@@ -45,52 +40,15 @@ const documents = [
     tags: ["products", "shop"],
     count: 47,
     reply: "lorem ipsum blablablba",
-    image: require("../assets/icons/fertilizers.png"),
   },
-];
-
-const products = [
-  {
-    id: 1,
-    name: "16 Best Plants That Thrive In Your Bedroom",
-    description:
-      "Bedrooms deserve to be decorated with lush greenery just like every other room in the house – but it can be tricky to find a plant that thrives here. Low light, high humidity and warm temperatures mean only certain houseplants will flourish.",
-    tags: ["Interior", "27 m²", "Ideas"],
-    images: [
-      require("../assets/images/plants_1.png"),
-      require("../assets/images/plants_2.png"),
-      require("../assets/images/plants_3.png"),
-      // showing only 3 images, show +6 for the rest
-      require("../assets/images/plants_1.png"),
-      require("../assets/images/plants_2.png"),
-      require("../assets/images/plants_3.png"),
-      require("../assets/images/plants_1.png"),
-      require("../assets/images/plants_2.png"),
-      require("../assets/images/plants_3.png"),
-    ],
-  },
-];
-
-const explore = [
-  // images
-  require("../assets/images/explore_1.png"),
-  require("../assets/images/explore_2.png"),
-  require("../assets/images/explore_3.png"),
-  require("../assets/images/explore_4.png"),
-  require("../assets/images/explore_5.png"),
-  require("../assets/images/explore_6.png"),
 ];
 
 const profile = {
   username: "react-ui-kit",
   location: "Europe",
   email: "contact@react-ui-kit.com",
-  avatar: require("../assets/images/avatar.png"),
   phone: "0812574387823",
-  budget: 1000,
-  monthly_cap: 5000,
   notifications: true,
-  newsletter: false,
 };
 
-export { documents, explore, products, profile };
+export { documents, profile };
